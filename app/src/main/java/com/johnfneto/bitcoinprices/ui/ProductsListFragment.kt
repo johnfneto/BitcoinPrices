31aec09ede42a4a0220932612b62c964197d19ac
John Neto <john.neto@optus.com.au>
2020-02-24 12:57:58 +1100

    ui setup

---
+++ app/src/main/java/com/johnfneto/bitcoinprices/ui/ProductsListFragment.kt
@@ -0,0 +1,16 @@
+package com.johnfneto.bitcoinprices.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import com.johnfneto.bitcoinprices.R
+
+class ProductsListFragment : Fragment(R.layout.fragment_products_list) {
+    private val TAG = javaClass.simpleName
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
