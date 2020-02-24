20c2cd5b2d37e2ed7cb55f8335b290c5262a5c8d
John Neto <john.neto@optus.com.au>
2020-02-24 12:31:16 +1100

    project setup

--- app/src/main/java/com/johnfneto/bitcoinprices/ui/MainActivity.kt
+++ app/src/main/java/com/johnfneto/bitcoinprices/ui/MainActivity.kt
@@ -1,11 +1,16 @@
-package com.johnfneto.bitcoinprices
+package com.johnfneto.bitcoinprices.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.johnfneto.bitcoinprices.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = javaClass.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
     }
 }
